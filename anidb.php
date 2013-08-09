05c7e1082ed3a39a53c4a3fb722b49c5cd6534d3
Ricky Burgin <ricky@burg.in>
2013-08-09 05:36:01 +0100

    Code and file serving improvements
    Created variables for filenames and AniDB URL for easier editing by
    end-users. Removed 303 redirect to locally stored file and instead
    instruct PHP to serve the file directly through the script itself.

--- anidb.php
+++ anidb.php
@@ -2,7 +2,7 @@
 
 /*
 
-   AniDB.net Simple XML Grabber 0.1 (2013/08/09)
+   AniDB.net Simple XML Grabber 0.2 (2013/08/09)
 
    Copyright 2013, Ricky Burgin
 
@@ -20,16 +20,20 @@
 
 */
 
+$xmlName = "anidb.xml"; // Desired filename
+$xmlFile = "./" . $xmlName; // Desired location for file
+$anidbXmlURL = "http://anidb.net/api/anime-titles.xml.gz"; // AniDB XML API URL (bloody acronyms)
+
 function getAnidbData() {
-        $c = curl_init("http://anidb.net/api/anime-titles.xml.gz"); // Init curl
-        $f = fopen("anidb.xml.gz", "w"); // Init file stream
+        $c = curl_init($anidbXmlURL . ".gz"); // Init curl
+        $f = fopen($xmlFile, "w"); // Init file stream
         curl_setopt($c, CURLOPT_FILE, $f); // Set curl output to file
         curl_setopt($c, CURLOPT_HEADER, 0);
         curl_exec($c); // Execute curl with params of $c
         curl_close($c);
         fclose($f); // Close streams
-        uncompress("anidb.xml.gz", "anidb.xml"); // Call uncompress function to gunzip the XML file, supply source and destination filepaths
-        unlink("anidb.xml.gz"); // Remove compressed version, as it is unnecessary
+        uncompress($xmlFile . ".gz", $xmlFile); // Call uncompress function to gunzip the XML file, supply source and destination filepaths
+        unlink($xmlFile . ".gz"); // Remove compressed version, as it is unnecessary
 }
 
 function uncompress($src, $dst) { // Take the source (compressed) and destination (uncompressed) filepaths
@@ -43,9 +47,18 @@ function uncompress($src, $dst) { // Take the source (compressed) and destinatio
     fclose($d);
 }
 
-if (file_exists("./anidb.xml")) // File exists
-        if (time() - filemtime("./anidb.xml") >= 86400) getAnidbData(); // As per AniDB guidelines, we must only request one copy per day (24 hours), as such, if the file is 24 hours old, or older at time of request, we call the getAnidbData() function to fetch a new copy.
+if (file_exists($xmlFile)) // File exists
+        if (time() - filemtime($xmlFile) >= 86400) getAnidbData(); // As per AniDB guidelines, we must only request one copy per day (24 hours), as such, if the file is 24 hours old, or older at time of request, we call the getAnidbData() function to fetch a new copy.
 
-header('Location: anidb.xml', false, 303); // Whether or not a new copy was required, we redirect the user to the locally hosted copy. We are working with relative directories, hence the lack of a domain and HTTP URI.
+header('Content-type: application/xml'); // Set MIME-type for XML
+header('Content-Description: AniDB XML');
+header('Content-Disposition: attachment; filename=' . $xmlName); // Force a download
+header('Expires: 0');
+header('Cache-Control: must-revalidate'); // Try to ensure client does not use a local cached copy
+header('Pragma: public');
+header('Content-Length: ' . filesize($xmlFile));
+ob_clean();
+flush();
+readfile($xmlFile);
 
 ?>
